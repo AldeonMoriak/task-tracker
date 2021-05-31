2a1781822dbc88c856d5755e6102c31ee081d555
Aldeon Arka <aldeonmoriak@gmail.com>
2021-05-31 17:13:04 +0430

    add header and tasks

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <div
+    class="mx-auto max-w-screen-md border border-blue-300 text-center h-screen"
+  >
+    <Header />
+    <Tasks msg="Hello Vue 3 + Vite" />
+  </div>
 </template>
 
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import Tasks from "./components/Tasks.vue";
+import Header from "./components/Header.vue";
 
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Tasks.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Hello</h1>
+  </div>
+</template>
+
+<script setup>
+import { defineProps, reactive } from "vue";
+
+defineProps({
+  msg: String,
+});
+
+const state = reactive({ count: 0 });
+</script>
