bf6362cbeabd86c358c6860dcd7bd1e96f779da7
Aldeon Arka <aldeonmoriak@gmail.com>
2021-06-03 13:17:51 +0430

    s

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,33 @@
   <div
     class="mx-auto max-w-screen-md border border-blue-300 text-center h-screen"
   >
-    <Header />
+    <Header v-on:task-inserted="addTask" />
     <Tasks msg="Hello Vue 3 + Vite" />
+    <div v-for="(task, index) in tasks" :key="index">{{ task }}</div>
   </div>
 </template>
 
-<script setup>
+<script>
 import Tasks from "./components/Tasks.vue";
 import Header from "./components/Header.vue";
+
+export default {
+  components: {
+    Tasks,
+    Header,
+  },
+  data() {
+    return {
+      tasks: [],
+    };
+  },
+  methods: {
+    addTask(value) {
+      this.tasks.push(value);
+      console.log(this.tasks);
+    },
+  },
+};
 
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,10 +2,11 @@
   <div>
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      class="h-6 w-6 text-pink-600 mx-12 mt-4"
+      class="h-6 w-6 text-pink-600 mx-12 mt-4 cursor-pointer"
       fill="none"
       viewBox="0 0 24 24"
       stroke="currentColor"
+      @click="showToggleHandler"
     >
       <path
         stroke-linecap="round"
@@ -14,5 +15,37 @@
         d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
       />
     </svg>
+    <input
+      type="text"
+      ref="task"
+      v-model="taskName"
+      class="border border-blue-300 ring ring-blue-300 rounded w-64 fixed top-4 left-1/3"
+      v-if="show"
+      @keypress.enter="insertTaskHandler"
+    />
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      show: false,
+      taskName: "",
+    };
+  },
+  methods: {
+    showToggleHandler() {
+      this.show = !this.show;
+      this.focusInput();
+      this.taskName = "";
+    },
+    insertTaskHandler() {
+      this.$emit("task-inserted", this.taskName);
+      this.showToggleHandler();
+    },
+    focusInput() {
+      this.$refs.task.focus();
+    },
+  },
+};
+</script>
