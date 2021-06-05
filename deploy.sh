a89cdfaaf3f5a36c3d52b4cae0eade809e45d277
lubuntuAldeon <mehranmirshekaran@protonmail.com>
2021-06-05 23:03:17 +0430

    change deploy settings

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:aldeonmoriak/aldeonmoriak.github.io.git main
+# git push -f git@github.com:aldeonmoriak/aldeonmoriak.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:aldeonmoriak/task-tracker.git main:gh-pages
 
 cd -
