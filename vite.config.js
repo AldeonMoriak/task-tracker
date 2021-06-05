8fc7e7f0e515a4b7788b199605f576d668ce5c69
lubuntuAldeon <mehranmirshekaran@protonmail.com>
2021-06-05 23:51:23 +0430

    on changing base

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import WindiCSS from "vite-plugin-windicss";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), WindiCSS()],
-  base: 'task-tracker'
+  base: '/task-tracker'
 });
