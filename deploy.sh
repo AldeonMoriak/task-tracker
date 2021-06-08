cd3658fe0a2372c4f1e1e802a6fa00d8ae5b4577
Aldeon Arka <aldeonmoriak@gmail.com>
2021-06-08 10:31:17 +0430

    change settings

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 set -e
 
 # build
-npm run build --base=/task-tracker-website/
+npm run build
 
 # navigate into the build output directory
 cd dist
@@ -12,13 +12,15 @@ cd dist
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
-git add -A
+git init
+git add .
 git commit -m 'deploy'
+git checkout -b gh-pages
 
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:aldeonmoriak/aldeonmoriak.github.io.git main
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:aldeonmoriak/task-tracker-website.git main
+git push -f https://github.com/AldeonMoriak/task-tracker.git gh-pages
 
 cd -
